b69fbcddc319667c71d56f203db1fa6f43ccd85b
Jeef <madih.jaafar@gmail.com>
2017-01-14 20:18:44 +0100

    Delete some features from this branch

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {SkillsService} from "./skills.service";
 import {Skills} from "./skills";
 import {Observable} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
@@ -13,30 +12,10 @@ export class SkillsComponent implements OnInit {
 
   skills : Observable<Skills[]>;
 
-  constructor(private _service : SkillsService, private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
     this.skills = this._route.snapshot.data['skills'];
   }
 
-  generateSkill(){
-    let id = new Date().getTime()+"";
-    let title =  "Angular 1";
-    let score = 80;
-    let img = "/assets/images/ng1.png";
-    let skill : Skills = new Skills();
-    skill.id = id;
-    skill.title = title;
-    skill.score = score;
-    skill.img = img;
-    this._service.add(skill);
-  }
-
-  delete(){
-
-  }
-
-  update(){
-
-  }
 }
