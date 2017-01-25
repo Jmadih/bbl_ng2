859ea63374b090e4f0fc908cc99e041edc76f8c8
Jeef <madih.jaafar@gmail.com>
2017-01-25 19:05:21 +0100

    Delete unused functions

--- _server/routes/data/skills.js
+++ _server/routes/data/skills.js
@@ -1,9 +1,14 @@
 'use strict';
 
 exports.skills = [
-    {
-        _img : "/assets/images/ng1.png",
-        _title: "Angular1",
-        _score: "90"
-    }
+  {
+    _img : "/assets/images/java.png",
+    _title: "Java",
+    _score: "80"
+  },
+  {
+    _img : "/assets/images/ng1.png",
+    _title: "Angular1",
+    _score: "90"
+  }
 ];
